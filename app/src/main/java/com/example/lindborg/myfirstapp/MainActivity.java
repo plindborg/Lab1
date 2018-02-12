97e34495dee560f849b83cff34d308b3b2b08904
Patrik Lindborg <lindborg@Patriks-MacBook-Pro.local>
2018-02-12 10:03:51 +0100

    fourth commit

--- app/src/main/java/com/example/lindborg/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/lindborg/myfirstapp/MainActivity.java
@@ -27,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
                     i = 0;
                 }
                 text1.setText(names[i]);
+                text1.setTextSize(20 + i);;
                 i++;
             }
         });
-
     }
-
 }
